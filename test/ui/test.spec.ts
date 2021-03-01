b6636a9a190de4efda3c2016c7ff62f0e10fd000
Sebastian Villegas Zapata <svillegasz@psl.com.co>
2021-03-01 08:55:33 -0500

    Make test fail. Refactor workflow: continue if test fails

--- test/ui/test.spec.ts
+++ test/ui/test.spec.ts
@@ -5,6 +5,6 @@ describe('This is the first example of protractor', () => {
   it('should have a title', async () => {
     browser.ignoreSynchronization = true;
     await browser.get('http://www.google.com');
-    expect(await browser.getTitle()).to.be.eql('Google');
+    expect(await browser.getTitle()).to.be.eql('Googla');
   });
 });
